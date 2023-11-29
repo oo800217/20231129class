14414dcb72ce5e6bbe021d2e5ad08f8dd5db1280
LE402Nt\uer <oo800217@gmail.com>
2023-11-29 17:17:23 +0800

    新增4格圖

--- 20231129.R
+++ 20231129.R
@@ -4,3 +4,11 @@ head(dat)
 
 
 hist(dat[,"eGFR"])   # 畫直方圖
+
+
+
+par(mfrow = c(2, 2))
+hist(dat[,"eGFR"], col = "red")
+boxplot(dat[,"eGFR"], col = "blue")
+pie(table(dat[,"Education"]), col = c("blue", "red", "green"))
+barplot(table(dat[,"Education"]), col = c("gray90", "gray50", "gray10"))
